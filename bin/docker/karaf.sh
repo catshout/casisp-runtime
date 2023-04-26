1105dab020d8dab175c67b88348e3218e58761f4
catshout <casisp@ip-10-0-0-193.eu-central-1.compute.internal>
2023-04-26 16:36:37 +0000

    Delete unnecessary files.

--- bin/docker/karaf.sh
+++
@@ -1,6 +0,0 @@
-feature:repo-add hawtio 2.17.0
-feature:repo-add activemq 5.17.4
-feature:repo-add camel 3.20.2
-feature:install pax-web-jetty
-feature:install hawtio activemq-broker-noweb camel camel-jms jms camel-http camel-servlet camel-swagger-java camel-ftp camel-jackson camel-jsonpath camel-json-validator camel-zipfile camel-velocity camel-groovy camel-salesforce camel-kafka
-
